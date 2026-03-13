cb1042033e3f916c99f5db83177cb0adc4d4e7a6
luiz <lhenrique.gsantos@gmail.com>
2026-03-13 09:36:22 +0100

    paralelização

---
+++ Vectorisation/renderer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include "fractal_land.hpp"
+#include "ant.hpp"
+#include "pheronome.hpp"
+#include "window.hpp"
+
+class Renderer
+{
+public:
+    Renderer(  const fractal_land& land, const pheronome& phen, 
+               const position_t& pos_nest, const position_t& pos_food,
+               const std::vector<int>& pos_x, 
+               const std::vector<int>& pos_y);
+
+    Renderer(const Renderer& ) = delete;
+    ~Renderer();
+
+    void display( Window& win, std::size_t const& compteur );
+private:
+    fractal_land const& m_ref_land;
+    SDL_Texture* m_land{ nullptr }; 
+    const pheronome& m_ref_phen;
+    const position_t& m_pos_nest;
+    const position_t& m_pos_food;
+    const std::vector<int>& m_ref_x;
+    const std::vector<int>& m_ref_y;
+    std::vector<std::size_t> m_curve;    
+};
